58798f008e42e01f714b953112a1f0141d92db18
Arthur <arthuromp000@gmail.com>
2020-12-27 20:17:02 -0300

    Add dashboard page to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/signin" component={SignIn} />
+    <Route path="/dashboard" component={Dashboard} isPrivate={true} />
   </Switch>
 );
 
