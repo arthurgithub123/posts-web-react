0fe1758a36c282b281d8440588d724f3e08b0da4
Arthur <arthuromp000@gmail.com>
2020-12-30 15:42:56 -0300

    Add /posts/create to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,13 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
+import CreatePost from '../pages/Post/Create';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/signin" component={SignIn} />
     <Route path="/dashboard" component={Dashboard} isPrivate={true} />
+    <Route path="/posts/create" component={CreatePost} isPrivate={true} />
   </Switch>
 );
 
