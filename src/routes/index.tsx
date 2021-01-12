f1ec49aabb1776e9ce347dc99a0b483c5c2a8a7e
Arthur <arthuromp000@gmail.com>
2021-01-12 14:24:37 -0300

    Add list route to Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,14 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import CreatePost from '../pages/Post/Create';
+import ListPost from '../pages/Post/List';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/signin" component={SignIn} />
     <Route path="/dashboard" component={Dashboard} isPrivate={true} />
     <Route path="/posts/create" component={CreatePost} isPrivate={true} />
+    <Route path="/posts/list" component={ListPost} isPrivate={true} />
   </Switch>
 );
 
