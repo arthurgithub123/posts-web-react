aef3549215bd61658307606f0b88d31a2a993025
Arthur <arthuromp000@gmail.com>
2021-01-15 00:11:35 -0300

    Add filters logic and spin for page loading

--- src/pages/Post/List/index.tsx
+++ src/pages/Post/List/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Post from '../../../components/Post';
-import { Col, notification, Row } from 'antd';
+import { Col, notification, Radio, Row, Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 import axiosConfiguration from '../../../axiosConfiguration/axiosConfigurations';
 
@@ -16,39 +17,70 @@ interface IPost {
 const ListPost: React.FC = () => {
 
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [filterValue, setFilterValue] = React.useState('all');
+  const [spinLoad, setSpinLoad] = useState(false);
 
   useEffect(() => {
-    axiosConfiguration.get<IPost[]>('api/Posts/List/all')
-      .then(posts => setPosts(posts.data))
+    setSpinLoad(true);
+
+    axiosConfiguration.get<IPost[]>('api/Posts/List/' + filterValue)
+      .then(posts => {
+        setPosts(posts.data);
+        setSpinLoad(false);
+      })
       .catch(err => {
+        setSpinLoad(false);
+
         notification['error']({
           message: 'Erro',
           description: err.response.data.Message,
           duration: 2
         });
       });
+  }, [filterValue]);
+
+  const handleFilterValueChange = useCallback((event) => {
+    setFilterValue(event.target.value);
   }, []);
 
   return (
     <>
-      {
-      posts &&
-        posts.map(post => (
-          <Row style={{ marginBottom: '30px' }}>
-            <Col md={7}></Col>
-            <Col md={10}>
-              <Post key={post.id}
-                initialText={post.description != '' ? post.description : undefined}
-                textareaStyle={!!post.description ? undefined : { display: 'none' }}
-                initialImageSrc={post.imageUrl}
-                readonlyImage
-                readonlyTextArea
-              />
-            </Col>
-            <Col md={7}></Col>
-          </Row>
-        ))
-      }
+      <Spin tip="Carregando..." size="large" indicator={<LoadingOutlined spin />} spinning={spinLoad}>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col md={7}></Col>
+          <Col md={10} style={{ textAlign: 'center' }}>
+            <Radio.Group onChange={handleFilterValueChange} value={filterValue}>
+              <ul>
+                <li><Radio value="all">Todos</Radio></li>
+                <li><Radio value="onlyAdministratorsPosts">Posts de administradores</Radio></li>
+                <li><Radio value="recomendedNotAccepted">Recomendados e não aceitos</Radio></li>
+                <li><Radio value="recomendedAndAccepted">Recomendados e aceitos</Radio></li>
+                <li><Radio value="recomendDate">Data de recomendação</Radio></li>
+                <li><Radio value="acceptedDate">Data de aceitação</Radio></li>
+              </ul>
+            </Radio.Group>
+          </Col>
+          <Col md={7}></Col>
+        </Row>
+        {
+          posts &&
+          posts.map(post => (
+            <Row style={{ marginBottom: '30px' }}>
+              <Col md={7}></Col>
+              <Col md={10}>
+                <Post key={post.id}
+                  initialText={post.description != '' ? post.description : undefined}
+                  textareaStyle={!!post.description ? undefined : { display: 'none' }}
+                  initialImageSrc={post.imageUrl}
+                  readonlyImage
+                  readonlyTextArea
+                />
+              </Col>
+              <Col md={7}></Col>
+            </Row>
+          ))
+        }
+      </Spin>
     </>
   );
 }
