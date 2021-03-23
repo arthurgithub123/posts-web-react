8a17277d633d74edfe229737136d748002bd8bca
Arthur <arthuromp000@gmail.com>
2021-03-23 17:10:17 -0300

    Change Create component code

--- src/pages/Post/Create/index.tsx
+++ src/pages/Post/Create/index.tsx
@@ -1,64 +1,24 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React from 'react';
 
-import { Button, Col, notification, Row } from 'antd';
 import Post from '../../../components/Post';
-import axiosConfiguration from '../../../axiosConfiguration/axiosConfigurations';
-import { useHistory } from 'react-router-dom';
+import { Col, Row } from 'antd';
 
 const CreatePost: React.FC = () => {
-
-  const history = useHistory();
   
-  const [selectedImage, setSelectedImage] = useState<File | undefined>();
-  const [textValue, setTextValue] = useState('');
-
-  const handleSetTextValue = useCallback((value) => { 
-    setTextValue(value); 
-  }, [textValue]);
-
-  const handlePostSubmit = useCallback(() => {
-
-    if(!selectedImage && !textValue) {
-      notification['error']({
-        message: 'Erro',
-        description: 'Preencha um dos campos',
-        duration: 2
-      });
-
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append('description', textValue);
-    if (selectedImage) formData.append('image', selectedImage);
-
-    axiosConfiguration.post('api/Posts/Create', formData)
-      .then(() => history.push('/posts/list'))
-      .catch(err => {
-        notification['error']({
-          message: 'Erro',
-          description: err.response.data.Message,
-          duration: 2
-        });
-      });
-    
-  }, [selectedImage, textValue]);
-
   return (
-    <>
-      <Row>
-        <Col md={7}></Col>
-        <Col md={10}>
-          <Post
-            setSelectedImageProp={setSelectedImage}
-            setTextProp={handleSetTextValue}
-            cancelButton
-          />
-          <Button type="primary" block onClick={handlePostSubmit}>Postar</Button>
-        </Col>
-        <Col md={7}></Col>
-      </Row>
-    </>
+    <Row>
+      <Col md={7}></Col>
+      <Col md={10}>
+        <Post
+          cancelButton
+          cancelButtonAlert
+          cancelButtonAlertMessage='Excluir imagem?'
+          cancelButtonTooltipMessage='Preencha um dos campos'
+          createPost
+        />
+      </Col>
+      <Col md={7}></Col>
+    </Row>
   );
 }
 
