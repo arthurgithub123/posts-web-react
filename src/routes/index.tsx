4afba697618f2d09088fc80d6f19932124db1845
Arthur <arthuromp000@gmail.com>
2021-03-23 17:17:35 -0300

    Add Edit component to Routes component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import CreatePost from '../pages/Post/Create';
 import ListPost from '../pages/Post/List';
+import EditPost from '../pages/Post/Edit';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -13,6 +14,7 @@ const Routes: React.FC = () => (
     <Route path="/dashboard" component={Dashboard} isPrivate={true} />
     <Route path="/posts/create" component={CreatePost} isPrivate={true} />
     <Route path="/posts/list" component={ListPost} isPrivate={true} />
+    <Route path="/posts/edit/:id" component={EditPost} isPrivate={true} />
   </Switch>
 );
 
