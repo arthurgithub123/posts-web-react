a9e31db2fba93126cd3103b34aac0e67fa06a0e4
Arthur <arthuromp000@gmail.com>
2021-04-14 17:41:40 -0300

    Fix ListPost Post props logic and added method for Administrators to accept posts

--- src/pages/Post/List/index.tsx
+++ src/pages/Post/List/index.tsx
@@ -1,11 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
 import Post from '../../../components/Post';
-import { Col, notification, Radio, Row, Spin } from 'antd';
-import { LoadingOutlined, EditOutlined } from '@ant-design/icons';
+import { Col, Modal, notification, Radio, Row, Spin } from 'antd';
+import { LoadingOutlined, EditOutlined, CheckSquareOutlined } from '@ant-design/icons';
 
 import axiosConfiguration from '../../../axiosConfiguration/axiosConfigurations';
 import { useHistory } from 'react-router-dom';
+import { useAuth } from '../../../hooks/auth';
 
 interface IPost {
   id: any;
@@ -18,11 +19,12 @@ interface IPost {
 
 const ListPost: React.FC = () => {
 
+  const { user } = useAuth();
   const history = useHistory();
   const [posts, setPosts] = useState<IPost[]>([]);
   const [filterValue, setFilterValue] = React.useState('all');
   const [spinLoad, setSpinLoad] = useState(false);
-
+  
   useEffect(() => {
     setSpinLoad(true);
 
@@ -46,6 +48,32 @@ const ListPost: React.FC = () => {
     setFilterValue(event.target.value);
   }, []);
 
+  const handleAccept = useCallback((id: string) => {
+  
+    Modal.confirm({
+      title: 'Aceitar post?',
+      onOk() {
+        axiosConfiguration.put(`api/Posts/Accept/${id}`)
+          .then(() => {
+            setFilterValue('acceptedDate');
+
+            notification['success']({
+              message: 'Post aceito',
+              duration: 2
+            });
+          })
+          .catch(err => {
+            Modal.error({
+              title: 'Erro',
+              maskClosable: true,
+              centered: true,
+              content: err.response.data
+            });
+          });
+      }
+    });    
+  }, []);
+
   return (
     <>
       <Spin tip="Carregando..." size="large" indicator={<LoadingOutlined spin />} spinning={spinLoad}>
@@ -67,41 +95,29 @@ const ListPost: React.FC = () => {
         </Row>
         {
           posts &&
-          posts.map(post => (
-            <Row style={{ marginBottom: '30px' }}>
-              <Col md={7}></Col>
-              <Col md={10}>
-                {
-                  post.canEdit
-                    ?
-                      <Post key={post.id}
-                        initialText={post.description !== '' ? post.description : undefined}
-                        textareaStyle={!!post.description ? undefined : { display: 'none' }}
-                        initialImageSrc={post.imageUrl}
-                        readonlyImage
-                        readonlyTextArea
-                        cardProps={
-                          {
-                            actions: [
-                              <EditOutlined key="edit" onClick={() => history.push(`/posts/edit/${post.id}`, { postId: post.id })} />
-                            ] 
-                          }
-                        }
-                    />
-                  :
-                    <Post key={post.id}
-                      initialText={post.description !== '' ? post.description : undefined}
-                      textareaStyle={!!post.description ? undefined : { display: 'none' }}
-                      initialImageSrc={post.imageUrl}
-                      readonlyImage
-                      readonlyTextArea
-                    />
-                }
-                
-              </Col>
-              <Col md={7}></Col>
-            </Row>
-          ))
+            posts.map(post => (
+              <Row style={{ marginBottom: '30px' }}>
+                <Col md={7}></Col>
+                <Col md={10}>
+                  <Post key={post.id}
+                    initialText={post.description !== '' ? post.description : undefined}
+                    textareaStyle={!!post.description ? undefined : { display: 'none' }}
+                    initialImageSrc={post.imageUrl}
+                    readonlyImage
+                    readonlyTextArea
+                    {
+                      ...post.canEdit &&
+                        {cardProps:{ actions: [<EditOutlined key="edit" onClick={() => history.push(`/posts/edit/${post.id}`, { postId: post.id })} />]}}
+                    }
+                    {
+                      ...user.role === 'Administrator' && filterValue === 'recomendedNotAccepted' &&
+                        {cardProps:{ actions: [<CheckSquareOutlined key="edit" onClick={() => handleAccept(post.id)} />]}}
+                    }
+                  />
+                </Col>
+                <Col md={7}></Col>
+              </Row>
+            ))
         }
       </Spin>
     </>
